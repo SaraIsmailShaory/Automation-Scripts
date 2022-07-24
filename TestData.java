c807d977731b932a29e04433f4a7bd7cc3a1d36b
SaraIsmailShaory <109699475+SaraIsmailShaory@users.noreply.github.com>
2022-07-24 13:59:00 +0200

    Add files via upload
    this File contains Test Data

---
+++ TestData.java
@@ -0,0 +1,22 @@
+package TestData;
+
+import org.openqa.selenium.WebDriver;
+
+public class TestData {
+	
+	public String FirstName="Sara";
+
+	public String LastName="Joseph";
+
+	public String Email="TestingSpecialist@yopmail.com";
+
+	public String Password="QqWwEeRr";
+
+	public String ConfirmPassword="QqWwEeRr";
+	
+
+	public WebDriver driver;
+	public String baseURL = "http://magento-demo.lexiconn.com/";
+	public String DriverPath = "C:\\Users\\sarat\\Downloads\\chromedriver.exe";
+
+}
